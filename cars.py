cf413091fe54fb8ce5557c23ee679331e123e35b
Yakub Almazyad <60103135@udst.edu.qa>
2024-01-31 12:01:26 +0300

    years added

--- cars.py
+++ cars.py
@@ -5,3 +5,12 @@ cars = ["Toyota", "Ford", "Honda", "Chevrolet", "Mercedes-Benz", "BMW", "Audi", 
 selected_brand = random.choice(cars)
 
 print(selected_brand)
+
+def modelYear():
+    year = ['2001', '2002', '2003', '2012', '2015', '2018', '2022', '2024']
+
+    select_year = random.choice(year)
+    
+    print(select_year)
+
+modelYear()
